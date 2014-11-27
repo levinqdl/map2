4eb10c45b23a84df2b08e4a00c4935eb57aaf6b1
root <levinqdl@gmail.com>
2014-11-28 00:25:52 +0800

    update

---
+++ index.js
@@ -0,0 +1,266 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var querystring = require("querystring");
+var async = require("async");
+
+/****mysql connection****/
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	database:'map',
+	//password:'',
+	password:'123456',
+	//port:3306
+	port:'/opt/lampp/var/mysql/mysql.sock'
+});
+conn.connect(function(err){
+	//connected!(unless 'err' is set)
+	if ( err ){
+		console.log(err);
+	}
+});
+
+
+var qm = new QueueManager();
+var looper = new Looper();
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/base.js', function(req, res){
+  res.sendFile(__dirname+'/js/base.js');
+});
+app.get('/jquery.js', function(req, res){
+  res.sendFile(__dirname+'/js/jquery-1.11.1.min.js');
+});
+app.get('/history', function(req, res){
+  res.sendFile(__dirname+'/history.html');
+});
+
+app.post('/call', function( req, res ){
+	var postData = ""; 
+	req.addListener("data", function (postDataChunk) {
+        postData += postDataChunk;
+    });
+    // 数据接收完毕，执行回调函数
+    req.addListener("end", function () {
+		//parse post data
+        var params = querystring.parse(postData);
+        console.log(params);
+        console.log(params["data"]);
+		
+		//parse json data
+		if ( params["json"] ){
+			var data = JSON.parse( params["json"] );
+			if ( data.n != 3 ){
+				qm.getQueueById(data.n).enqueue(params['json']); // dispatch and enqueue
+				
+				for ( var key in data ){
+					console.log( key+":"+data[key] );
+				}
+				console.log('data received');
+			} else {
+				io.emit('target', params['json']);
+			}
+		}
+		
+		// qm.getQueueById(data.N).enqueue( data.value );
+        res.writeHead(500, {
+            "Content-Type": "text/plain;charset=utf-8"
+        });
+        res.end("call success\n");
+        
+    });
+});
+
+app.post('/car', function( req, res ){
+	var n = req.query.n;
+	var postData = ""; 
+	req.addListener("data", function (postDataChunk) {
+        postData += postDataChunk;
+    });
+    // 数据接收完毕，执行回调函数
+    req.addListener("end", function () {
+		//parse post data
+        var params = querystring.parse(postData);
+		
+		//parse json data
+		if ( params["json"] ){
+			console.log('info '+n+' received');
+			var json = params["json"];
+			io.emit('info', {n:n, json:json});
+			//clear lost connection timer
+			if ( qm.timer[n] ){
+				clearTimeout(qm.timer[n]);
+			}
+			console.log('setTimeout,'+n);
+			qm.timer[n] = setTimeout(notifyLost, 15000, n);
+		}
+		
+        res.writeHead(500, {
+            "Content-Type": "text/plain;charset=utf-8"
+        });
+        res.end("car success\n");
+        
+    });
+});
+
+
+var lastStrPoly = null;
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('store', function(msg){
+console.log("*****************************store*******************");	
+console.log(msg);
+  var strPoly = JSON.stringify(msg.polygon);
+  var target = JSON.stringify(msg.target);
+  if ( strPoly != lastStrPoly ){
+	conn.query('INSERT INTO data ( time, polygon, target ) VALUES('+msg.time+',\''+strPoly+'\',\''+target+'\')', function(err, rows, fields) {
+		console.log('store:'+rows+'rows');
+		console.log(err);
+	})
+	lastStrPoly = strPoly;
+  }
+  });
+  socket.on('history', function(msg){
+	
+	conn.query('SELECT * FROM data ORDER BY id DESC limit 44 ', function( err, rows, fields ){
+		if ( err ){
+			console.log(err);
+			io.emit('history_return', 'error');
+		} else {
+			io.emit('history_return', rows);
+		}
+	});
+	//delete old records
+	conn.query('SELECT id FROM data ORDER BY id DESC limit 50, 1', function( err, rows, fields ){
+		console.log(rows);
+		if ( rows.length ){
+			conn.query('DELETE FROM data WHERE id <= '+rows[0].id, function( err, rows, fields){
+				console.log( rows );
+			});
+		}
+	});
+  })
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
+
+//Queue
+function Queue(size){
+	this.size = size;
+	this.head = 0;
+	this.queue = new Array(this.size);
+	this.tail = 0;
+}
+Queue.prototype.enqueue = function(obj){
+	var tail = (this.tail+1)%this.size;
+	if ( tail == this.head ){
+		return false;
+	} else {
+		this.queue[this.tail] = obj;
+		this.tail = tail;
+	}
+}
+Queue.prototype.dequeue = function(){
+	var tmp;
+	if ( this.tail == this.head ){
+		return false;
+	} else {
+		tmp = this.queue[this.head];
+		this.head = (this.head+1)%this.size;
+	}
+	return tmp;
+}
+Queue.prototype.isEmpty = function(){
+	if ( this.tail == this.head ){
+		return true;
+	}
+	return false;
+}
+Queue.prototype.getHead = function(){
+	return this.queue[this.head];
+}
+
+//Queue Manager
+function QueueManager(){
+	this.queueTable = {};
+	this.timer = new Array(3);
+}
+QueueManager.prototype.getQueueById = function(id){
+	if ( typeof this.queueTable[id] == 'undefined' ){
+		this.queueTable[id] = new Queue( 50 );
+	}
+	return this.queueTable[id];
+}
+
+function Looper ( ){
+	this.flag = false;
+}
+Looper.prototype.fetch = function(cb){
+	var table = qm.queueTable, keeper=[], result=[];
+	for(var key in table ){
+		var queue = table[key];
+		if ( queue.isEmpty() == false ){
+			var head = queue.getHead();
+			
+			var time = JSON.parse(head).time;
+			time = new Date().setHours(time.substr(0,2),time.substr(2,2),time.substr(4,2));
+			time = time / 1000;
+			keeper.push({key:key,time:time});
+		}
+	}
+	if ( keeper.length ){
+	
+	broadcast(keeper);
+	}
+	console.log('keeper length:'+keeper.length);
+	/*
+	if ( keeper.length == 3 ){
+		keeper.sort( function( a, b){
+			return a.time - b.time;
+		});
+		var end = keeper.length-1;
+		while ( end > 0 ){
+			console.log(keeper);
+			if ( keeper[end].time - keeper[0].time <= range ){
+				broadcast(keeper);
+				break;
+			} else {
+				end--;
+			}
+			if ( end == 0 ){
+				console.log('en');
+				qm.queueTable[keeper[0].key].dequeue();
+			}
+		}
+	}
+	*/
+	setTimeout(cb, 200);
+}
+function broadcast( arr ){
+	console.log('***************************result call****************');
+	var r = [];
+	for ( var i=0; i < arr.length; i++ ){
+		var queue = qm.queueTable[arr[i].key];
+		r.push(queue.dequeue());
+	}
+	io.emit('update', r);
+}
+
+function notifyLost(n){
+	console.log('notifyLost,'+n);
+	io.emit('car_lost', n);
+}
+
+async.forever( looper.fetch, function( err ){
+	console.log(err);
+});
---
+++ js/base.js
@@ -0,0 +1,261 @@
+var projection = new BMap.MercatorProjection();
+function lngLatToPoint( x, y){
+	return projection.lngLatToPoint(new BMap.Point(x, y));
+}
+function pointToLngLat( x, y){
+	return projection.pointToLngLat(new BMap.Pixel(x, y));
+}
+function getPoint ( x, y ){
+	x = Math.floor(x/100) + ( x - Math.floor(x/100) * 100 ) / 60;
+	y = Math.floor(y/100) + ( y - Math.floor(y/100) * 100 ) / 60;
+	gpsPoint = new BMap.Point(x,y);
+	var dtd = $.Deferred();
+	BMap.Convertor.translate(gpsPoint,0,function(point){
+		dtd.resolve();
+	});
+	return dtd.promise();
+}
+function Point( x, y, lngLat, gps ){
+	this.dtd = $.Deferred();
+	if ( lngLat ){
+		if ( gps ){
+			x = Math.floor(x/100) + ( x - Math.floor(x/100) * 100 ) / 60;
+			y = Math.floor(y/100) + ( y - Math.floor(y/100) * 100 ) / 60;
+			gpsPoint = new BMap.Point(x,y);
+			var obj = this;
+			BMap.Convertor.translate(gpsPoint,0,function(point){
+				obj.bmap = point;
+				obj.pixel = lngLatToPoint( point.lng, point.lat );
+				obj.dtd.resolve();
+			});
+		} else {
+			this.bmap = new BMap.Point(x, y);//经纬坐标表示
+			this.pixel = lngLatToPoint(x, y);//平面坐标表示
+			this.dtd.resolve();
+		}
+	} else {
+		this.pixel = new BMap.Pixel(x, y);
+		this.bmap = pointToLngLat(x,y);
+		this.dtd.resolve();
+	}
+}
+Point.prototype.draw = function(config){
+	this.marker = new BMap.Marker( this.bmap, config );
+	map.addOverlay(this.marker);
+}
+Point.prototype.remove = function(){
+	map.removeOverlay(this.marker);
+}
+
+//线定义，传入点斜式：y-y1=k(x-x1)，转为一般式：ax+by+c=0
+//x,点的横坐标，实为经度
+//y,点的纵坐标，实为纬度
+//k,斜率
+function Line ( p, ang, config, endPoint ){
+	var k;
+	
+	this.p = p;
+	this.config = config;
+	if ( endPoint ){
+		this.endPoint = endPoint;
+		if ( endPoint.pixel.x - p.pixel.x == 0 ){
+			k = null;
+		} else {
+			k = ( endPoint.pixel.y - p.pixel.y ) / ( endPoint.pixel.x - p.pixel.x );
+		}
+	} else if ( ang ){
+		this.ang = ang *0.017453293; //转为弧度
+		if ( this.ang == 0 ){
+			k == null;
+		} else {
+			k = slope(this.ang);
+		}
+		var x = this.p.pixel.x + Math.sin(this.ang);
+		var y = this.p.pixel.y + Math.cos(this.ang)
+		this.endPoint = new Point(x, y );
+	}
+	
+	if ( k === null ){
+		this.a = 1;
+		this.b = 0;
+		this.c = -p.pixel.x;
+	} else if ( k === 0 ){
+		this.a = 0;
+		this.b = 1;
+		this.c = -p.pixel.y;
+	} else {
+		this.a = k;
+		this.b = -1;
+		this.c = p.pixel.y-k*p.pixel.x;
+	}
+}
+Line.prototype.draw = function( length ){
+	//this.p.draw();
+	var x = this.p.pixel.x + length * Math.sin(this.ang);
+	var y = this.p.pixel.y + length * Math.cos(this.ang);
+	point = new Point( x, y, false);
+	this.polyline = new BMap.Polyline([    
+		this.p.bmap,
+		point.bmap
+	],
+	{strokeColor:this.config.lineColor, strokeWeight:2, strokeOpacity:0.5}    
+	);
+	map.addOverlay(this.polyline);
+}
+Line.prototype.remove = function( ){
+	map.removeOverlay( this.polyline);
+}
+//计算两条直线交点
+Line.prototype.intersection = function( line ){
+	if ( this.a * line.b == line.a * this.b ){
+		return false;
+	} else {
+		var x = (line.b*this.c - line.c* this.b)/ ( line.a*this.b - this.a * line.b);
+		var y = (this.a * line.c - this.c * line.a)/ ( line.a* this.b - this.a * line.b);
+		return new Point(x, y, false);
+	}
+}
+Line.prototype.insideClipEdge = function(point, direction){
+	var A,B;
+	A = this.endPoint;
+	B = this.p;
+	var value = (B.pixel.x - A.pixel.x )*( point.pixel.y - A.pixel.y ) - ( B.pixel.y - A.pixel.y )*( point.pixel.x - A.pixel.x);
+	if ( direction == 1 ){
+		value = - value;
+	}
+	if ( value >= 0){
+		return true;
+	} else if ( value < 0 ){
+		return false;
+	}
+}
+
+function slope( ang ){
+	return 1/Math.tan ( ang );
+}
+
+function Sector ( p, ang, range, config){
+	this.p = p;
+	this.lines = [new Line( p, ang - range ,config), new Line( p, ang + range, config )];
+}
+//绘制扇形区域
+Sector.prototype.draw = function(length){
+	this.p.draw();
+	for ( i in this.lines ){
+		this.lines[i].draw(length);
+	}
+}
+Sector.prototype.remove = function(){
+	this.p.remove();
+	for ( i in this.lines ){
+		this.lines[i].remove();
+	}
+}
+Sector.prototype.intersection = function( sector ){
+	var p, polygonPoints = [], i=[0,1,1,0], j = [0,0,1,1];
+	var polygon = new Polygon();
+	for ( var index = 0; index <= 3; index++ ){
+		p = this.lines[i[index]].intersection( sector.lines[j[index]] );
+		if ( p !== false ){
+			polygon.path.push(p);
+		}
+	}
+	return polygon;  //创建多边形
+}
+//http://en.wikipedia.org/wiki/Sutherland%E2%80%93Hodgman_algorithm
+Sector.prototype.clip = function ( polygon ){
+	var input = polygon.path, output, E,S,i;
+	for ( i in this.lines ){
+		S = input.length-1;
+		output = [];
+		for ( E in input ){
+			//console.log('line:'+i);
+			//console.log('E:'+E+' '+this.lines[i].insideClipEdge(input[E], i));
+			//console.log(input);
+			if ( this.lines[i].insideClipEdge(input[E], i) ){
+				if ( this.lines[i].insideClipEdge(input[S], i ) == false ){
+					output.push( this.lines[i].intersection( new Line(input[S], null, null, input[E])));
+				}
+				output.push( input[E] );
+			} else if ( this.lines[i].insideClipEdge( input[S] , i ) ){
+				output.push( this.lines[i].intersection(new Line(input[S], null, null, input[E])));
+			}
+			S = E;
+		}
+		input = output;
+	}
+	polygon = new Polygon();
+	polygon.path = input;
+	return polygon;
+}
+
+function Polygon (){
+	this.path = [];
+}
+Polygon.prototype.draw = function(){
+	var path = [], i;
+	for ( i in this.path ){
+		path.push( this.path[i].bmap);
+		//this.path[i].draw();
+	}
+	this.bmap = new BMap.Polygon(path, {strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5, fillColor:'yellow'});
+	map.addOverlay(this.bmap);
+}
+Polygon.prototype.remove = function(){
+	map.removeOverlay( this.bmap );
+}
+
+function InfoControl(id){
+    this.defaultAnchor = BMAP_ANCHOR_TOP_RIGHT;    
+    this.defaultOffset = new BMap.Size(10, 10);
+	this.id = id;
+}    
+InfoControl.prototype = new BMap.Control();
+InfoControl.prototype.initialize = function(map){    
+	var div = document.createElement("div");
+	
+	div.id = this.id;
+	div.style.border = "1px solid gray";
+	div.style.backgroundColor = "black";
+ // 添加DOM元素到地图中   
+ map.getContainer().appendChild(div);    
+ // 将DOM元素返回
+ return div;
+ }
+ InfoControl.prototype.hide = function( direction ){
+	var offset = 0;
+	var div = $('#'+this.id);
+	var transition = 0;
+	div.addClass('infoControl');
+	if ( direction == 'right' ){
+		div.addClass('landscape');
+		offset = $(window).width();
+		direction = 'left';
+		transition = div.width();
+	} else if ( direction == 'bottom' ){
+		div.addClass('portait');
+		offset = $(window).height();
+		direction = 'top';
+		transition = div.height();
+	}
+	var style = {};
+	style[direction] = offset - 30;
+	div.offset(style);
+	var ani_style = {};
+	ani_style[direction] = offset - transition;
+	div.hover(function(){
+		div.animate(ani_style);
+	},function(){
+		div.animate(style);
+	})
+ }
+ InfoControl.prototype.setInfo = function ( info ){
+	this.info = info;
+ }
+ InfoControl.prototype.update = function (tpl){
+	for ( i in this.info ){
+		tpl = tpl.replace('{'+i.toUpperCase()+'}', this.info[i]);
+	}
+	$('#'+this.id).empty();
+	$('#'+this.id).append(tpl);
+ }
